0e929c81c0f44a7b53c0b502bde14cd122ca11f0
joseevilasio <jose_evilasio@outlook.com>
2023-10-17 15:36:36 +0100

    create hash and cli command create-user

--- dundie/models/user.py
+++ dundie/models/user.py
@@ -1,6 +1,7 @@
 """User related data models"""
 from typing import Optional
 from sqlmodel import Field, SQLModel
+from dundie.security import HashedPassword
 
 
 class User(SQLModel, table=True):
@@ -11,7 +12,7 @@ class User(SQLModel, table=True):
     username: str = Field(unique=True, nullable=False)
     avatar: Optional[str] = None
     bio: Optional[str] = None
-    password: str = Field(nullable=False)
+    password: HashedPassword
     name: str = Field(nullable=False)
     dept: str = Field(nullable=False)
     currency: str = Field(nullable=False)
@@ -21,3 +22,9 @@ class User(SQLModel, table=True):
         """Users belonging to management dept are admins."""
         return self.dept == "management"
 
+
+
+def generate_username(name: str) -> str:
+    """Generates a slug username from a name"""
+    #TODO: Utilizar slugify
+    return name.lower().replace(" ", "-")
