b6b24152a2546fe0148a5c5d58b00fb6be4661f4
joseevilasio <jose_evilasio@outlook.com>
2023-11-24 20:05:28 +0000

    add rotas transações, paginacao e filtros

--- dundie/routes/user.py
+++ dundie/routes/user.py
@@ -10,7 +10,11 @@ from dundie.models.user import User, UserRequest, UserResponse, UserProfilePatch
 from dundie.auth import SuperUser, AuthenticatedUser, CanChangeUserPassword
 from dundie.tasks.user import try_to_send_pwd_reset_email
 
-
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import parse_obj_as
+from dundie.auth import ShowBalanceField
+from dundie.models.user import UserResponseWithBalance
 
 router = APIRouter()
 
@@ -20,6 +24,46 @@ async def list_users(*, session: Session = ActiveSession):
     """List all users."""
     users = session.exec(select(User)).all()
     return users
+
+
+@router.get(
+    "/",
+    response_model=List[UserResponse] | List[UserResponseWithBalance],
+    response_model_exclude_unset=True,
+)
+async def list_users(
+    *, session: Session = ActiveSession, show_balance_field: bool = ShowBalanceField
+):
+    """List all users.
+
+    NOTES:
+    - This endpoint can be accessed with a token authentication
+    - show_balance query parameter takes effect only for authenticated superuser.
+    """
+    users = session.exec(select(User)).all()
+    if show_balance_field:
+        users_with_balance = parse_obj_as(List[UserResponseWithBalance], users)
+        return JSONResponse(jsonable_encoder(users_with_balance))
+    return users
+
+# get user by username 
+@router.get(
+    "/{username}/",
+    response_model=UserResponse | UserResponseWithBalance,
+    response_model_exclude_unset=True,
+)
+async def get_user_by_username(
+    *, session: Session = ActiveSession, username: str, show_balance_field: bool = ShowBalanceField
+):
+    """Get user by username"""
+    query = select(User).where(User.username == username)
+    user = session.exec(query).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if show_balance_field:
+        user_with_balance = parse_obj_as(UserResponseWithBalance, user)
+        return JSONResponse(jsonable_encoder(user_with_balance))
+    return user
 
 
 @router.get("/{username}/", response_model=UserResponse)
